47d86246a2940723e9733e6f4af4e429eb6535fe
Jesse Bryan and Michael Woldemedihin <michael@>
2017-07-17 11:45:29 -0700

    add the ability to delete specific items on the list

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,12 @@ ToDoList.prototype.fullTask = function(){
   return this.taskName + " on " + this.whenToDo;
 }
 
+
 $(document).ready(function(){
+
   $("form#new-task").submit(function(event){
     event.preventDefault();
-
+    //var toDoListArr = [];
     var taskName = $("#new-task-name").val();
     var when = $("#when").val();
     var description = $("#description").val();
@@ -20,21 +22,52 @@ $(document).ready(function(){
 
     var toDoList = new ToDoList(taskName, when, description, priority);
 
+//
+    // toDoListArr.push(toDoList);
+    // for (var i = 0; i < toDoListArr.length; i++) {
+    //   $("#todoList").append("<li> <span class='fakeLink'> " + toDoListArr[i].fullTask() + "</span> </li>");
+    //
+    // }
+//
     $("#todoList").append("<li> <span class='fakeLink'> " + toDoList.fullTask() + "</span> </li>");
 
     $("#todoList li").last().click(function(){
-
       $("#show-todoList").show();
       $("#show-todoList h2").text(toDoList.fullTask());     $(".new-task-name").text(taskName);
       $(".when").text(when);
       $(".description").text(description);
       $(".task-priority").text(priority);
+      // var index = $("#todoList li").indexOf(this);e
+      var itemToRemove = this;
+      $("#delete").off("click");
+      $("#delete").click(function(){
+        $("#show-todoList").hide();
+        itemToRemove.remove();
+      });
     });
 
-    $("#delete").last().click(function(){
-      $("#show-todoList").hide();
-      $("#todoList li").remove();
-    })
+  //  $("#delete").last().click(function(){
+
+//       //
+// debugger;
+//        var dtName = $(".new-task-name").val();
+//        var dWhen = $(".when").val();
+//        var dDescription = $(".description").val();
+//        var dPriority = $(".task-priority").val();
+//        var elementToDelete = new ToDoList(dtName, dWhen, dDescription, dPriority);
+//        var indexToDelete = toDoListarr.indexOf(elementToDelete);
+//        toDoListarr.splice(indexToDelete, 1);
+//
+//        for (var i = 0; i < toDoListArr.length; i++) {
+//          $("#todoList").append("<li> <span class='fakeLink'> " + toDoListArr[i].fullTask() + "</span> </li>");
+//
+//        }
+//
+//       //
+
+    //   $("#show-todoList").hide();
+    //   $("#todoList li").remove();
+    // })
 
     $("#new-task-name").val('');
     $("#when").val('');
